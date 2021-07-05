80d313323fcbd2718f2dcbd238b350a926cee7d9
ysung327 <ysung327@naver.com>
2021-07-05 21:01:27 +0900

    edit databases name

--- board/urls.py
+++ board/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from board.views import MeetingList, MeetingDetail, AddMeeting
 
 urlpatterns = [
-    path('/', MeetingList.as_view()),
+    path('', MeetingList.as_view()),
     path('add/', AddMeeting.as_view()),
     path('<int:pk>/', MeetingDetail.as_view()),
 ]
